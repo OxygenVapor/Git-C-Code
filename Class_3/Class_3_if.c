e33c728922611369966204d29b0ffa8c8b6b61fb
Oxygen_Vapor <m17520580153@gmail.com>
2025-03-17 23:04:08 +0800

    Class_3

--- Class_3/Class_3_if.c
+++ Class_3/Class_3_if.c
@@ -42,22 +42,50 @@
 //如果年龄45岁至59岁，打印 中老年
 //如果年龄60岁至89岁，打印 老年
 //如果年龄大于等于90岁，打印 老寿星
+//int main()
+//{
+//	int age = 0;
+//    scanf("%d", &age);
+//	if (age <= 0)
+//		printf("非法输入\n");
+//		else if(age < 18)
+//			printf("少年\n");
+//		else if(age <= 45)
+//			printf("青年\n");
+//		else if(age <= 59)
+//			printf("中老年\n");
+//		else if(age <= 89)
+//			printf("老年\n");
+//	else
+//        printf("老寿星\n");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age >0)
+//		if (age >= 18 && age <= 36)
+//		printf("青年\n");
+//		else
+//        printf("非青年\n");
+//	else
+//        printf("非法输入\n");	
+//	return 0;
+//}
+
 int main()
 {
-	int age = 0;
-    scanf("%d", &age);
-	if (age <= 0)
-		printf("非法输入\n");
-		else if(age < 18)
-			printf("少年\n");
-		else if(age <= 45)
-			printf("青年\n");
-		else if(age <= 59)
-			printf("中老年\n");
-		else if(age <= 89)
-			printf("老年\n");
-	else
-        printf("老寿星\n");
-
+	int year = 0;
+    scanf("%d", &year);
+	if (year > 0)
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+			printf("闰年\n");
+		else
+			printf("非闰年\n");
+    else
+        printf("非法输入\n");
 	return 0;
 }
