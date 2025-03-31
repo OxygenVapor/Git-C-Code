a74690639ce23ecbe55aa3cb193b5e7b84e01535
Oxygen_Vapor <m17520580153@gmail.com>
2025-03-31 23:17:13 +0800

    Class_6_Funtion

--- Class_6_Function/Class_6_Funtion.c
+++ Class_6_Function/Class_6_Funtion.c
@@ -38,28 +38,51 @@
 //	return 0;
 //}
 
-int set_Arr(int x[10], int y)
+//输入数组的数值
+int set_Arr(int x[10], int y, int z)
 {
 	for (int i = 0; i < y; i++)
 	{
-		x[i] = -1;
+		x[i] = z;
 	}
-	
 }
 
-int main()
+//打印数组
+void print_Arr(int x[10], int y)
 {
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	int arr_size = sizeof(arr) / sizeof(arr[0]);
-	for (int i = 0; i < arr_size; i++)
+	for (int i = 0; i < y; i++)
 	{
-		printf("%d ", arr[i]);
+		printf("%d ", x[i]);
 	}
 	printf("\n");
-	set_Arr(arr, arr_size);
-	for (int i = 0; i < arr_size; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	return 0;
 }
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int arr_size = sizeof(arr) / sizeof(arr[0]);
+//	print_Arr(arr, arr_size);
+//	set_Arr(arr, arr_size, -1);
+//	print_Arr(arr, arr_size);
+//	return 0;
+//}
+
+//打印二维数组
+void print_Arr_2d(int arr[3][5], int row, int column)
+{
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+	}
+	printf("\n");
+}
+//int main()
+//{
+//	int arr[3][5] = { 1,2,3,4,5, 6,7,8,9,10, 11,12,13,14,15 };
+//	print_Arr_2d(arr, 3, 5);
+//
+//	return 0;
+//}
