e07e03d63a73a11ffd0271c9ef2b365a6dc69f81
Oxygen_Vapor <m17520580153@gmail.com>
2025-04-01 07:45:03 +0800

    Class_6_Function

--- Class_6_Function/Class_6_Funtion.c
+++ Class_6_Function/Class_6_Funtion.c
@@ -76,8 +76,8 @@ void print_Arr_2d(int arr[3][5], int row, int column)
 		{
 			printf("%d ", arr[i][j]);
 		}
+		printf("\n");
 	}
-	printf("\n");
 }
 //int main()
 //{
@@ -86,3 +86,40 @@ void print_Arr_2d(int arr[3][5], int row, int column)
 //
 //	return 0;
 //}
+
+int Leap_year(int year)
+{
+	if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int get_day_of_month(int year, int month)
+{
+	int m[13] = { 0, 31,28,31,30,31,30,31,31,30,31,30,31 };
+	int leap = Leap_year(year);
+	if (leap == 1)
+	{
+		m[2] = 29;
+		return m[month];
+	}
+	else
+	{
+		return m[month];
+	}
+}
+
+int main()
+{
+	int year = 0;
+	int month = 0;
+	scanf("%d%d", &year, &month);
+	int day = get_day_of_month(year, month);
+	printf("%d 年 %d 月有 %d 天\n", year, month, day);
+	return 0;
+}
