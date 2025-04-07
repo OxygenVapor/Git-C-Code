546e5abe75070db8ec5beccd9d9478b67168a026
Oxygen_Vapor <m17520580153@gmail.com>
2025-04-08 00:25:33 +0800

    Class_6_Minesweeper

--- Class_6_Function/Minesweeper.c
+++ Class_6_Function/Minesweeper.c
@@ -1,3 +1,17 @@
 #define _CRT_SECURE_NO_WARNNINGS 1
 #include <stdio.h>
 
+void test() {
+	static int a = 0;
+	a++;
+	printf("%d ", a);
+}
+
+int main() {
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		test();
+	}
+	return 0;
+}
